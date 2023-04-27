9c5fe8c129dee3ea1f8a0904fefba220a56c0bb4
Dheeraj Patidar <88872393+dheerajpatidar212@users.noreply.github.com>
2023-04-27 15:27:50 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,36 @@
+<?php
+	$dbServer = 'localhost';
+	$dbUsername = 'msgsjmgp_dheeraj';
+	$dbPassword = 'RJ&o]Utb,_Dv';
+	$dbDatabase = 'msgsjmgp_jisort_db';
+
+	$adminConfig = array(
+		'adminUsername' => "dheeraj212",
+		'adminPassword' => "f4fc8a416f8be148db91d57412cc34a0",
+		'notifyAdminNewMembers' => "0",
+		'defaultSignUp' => "1",
+		'anonymousGroup' => "anonymous",
+		'anonymousMember' => "guest",
+		'groupsPerPage' => "10",
+		'membersPerPage' => "10",
+		'recordsPerPage' => "10",
+		'custom1' => "Course",
+		'custom2' => "",
+		'custom3' => "",
+		'custom4' => "",
+		'MySQLDateFormat' => "%m/%d/%Y",
+		'PHPDateFormat' => "n/j/Y",
+		'PHPDateTimeFormat' => "m/d/Y, h:i a",
+		'senderName' => "Membership management",
+		'senderEmail' => "admin@dheerajpatidar.com",
+		'approvalSubject' => "Your membership is now approved",
+		'approvalMessage' => "Dear member,\r\n\r\nYour membership is now approved by the admin. You can log in to your account here:\r\nhttp://localhost/jisort\r\n\r\nRegards,\r\nAdmin",
+		'hide_twitter_feed' => "",
+		'maintenance_mode_message' => "<b>Our website is currently down for maintenance</b><br>\r\nWe expect to be back in a couple hours. Thanks for your patience.",
+		'mail_function' => "mail",
+		'smtp_server' => "",
+		'smtp_encryption' => "",
+		'smtp_port' => "25",
+		'smtp_user' => "",
+		'smtp_pass' => ""
+	);
